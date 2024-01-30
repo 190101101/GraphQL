6ac033eb8f0d276d1228f064c64754658e1ab8f9
190101101 <190101101@mail.ru>
2024-01-30 19:10:47 +0400

    07 easysnap\74 bcrypt

---
+++ base/07 easysnap/74 bcrypt/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { importSchema } = require("graphql-import");
+const resolvers = require("./graphql/resolvers/index");
+const {User} = require("./models/index");
+
+mongoose
+  .connect(process.env.easysnap)
+  .then((response) => console.log("connected to easysnap"))
+  .catch((error) => console.log(error));
+
+const server = new ApolloServer({
+  typeDefs: importSchema("./src/graphql/schema.graphql"),
+  resolvers: resolvers,
+  context:{User}
+});
+
+const app = express();
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () => console.log(`http://localhost:4000/graphql`));
