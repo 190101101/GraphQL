077aae986551824d2166ee66c987e890fa0ac313
190101101 <190101101@mail.ru>
2024-01-31 15:47:27 +0400

    09 snap client\92 routing

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,12 +1,25 @@
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Join from "./pages/Join";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div id="app">
       <div class="container">
         <Header />
-        <Home />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/join" component={Join} />
+            <Route path="/login" component={Login} />
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     </div>
   );
