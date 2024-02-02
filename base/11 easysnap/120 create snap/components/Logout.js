fdcd610532b7e5e6e899d4facb52773420a675bc
190101101 <190101101@mail.ru>
2024-02-02 21:58:38 +0400

    11 easysnap\120 create snap

---
+++ base/11 easysnap/120 create snap/components/Logout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { ApolloConsumer } from "@apollo/client";
+
+const Logout = ({ history }) => {
+  const onClick = (client, history) => {
+    client.resetStore();
+    localStorage.removeItem("token");
+    history.push("/");
+  };
+
+  return (
+    <ApolloConsumer>
+      {(client) => {
+        return <button onClick={() => onClick(client, history)}>logout</button>;
+      }}
+    </ApolloConsumer>
+  );
+};
+
+export default withRouter(Logout);
