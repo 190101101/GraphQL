7513cdbd94a7d694c35feb0eea83ec269efb4c9b
190101101 <190101101@mail.ru>
2024-02-06 13:25:16 +0400

    13 subscriptions server\136 with filter

---
+++ base/13 subscriptions server/136 with filter/server.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const http = require("http");
+const mongoose = require("mongoose");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { ApolloServer, PubSub } = require("apollo-server-express");
+const { importSchema } = require("graphql-import");
+const resolvers = require("./graphql/resolvers/index");
+const { User, Snap } = require("./models/index");
+
+mongoose
+  .connect(process.env.easysnap)
+  .then((response) => console.log("connected to easysnap"))
+  .catch((error) => console.log(error));
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs: importSchema("./src/graphql/schema.graphql"),
+  resolvers: resolvers,
+  context: ({ req }) => ({
+    pubsub,
+    User,
+    Snap,
+    activeUser: req ? req.activeUser : null,
+  }),
+  introspection: true
+});
+
+const app = express();
+
+app.use((req, res, next) => {
+  const token = req.headers["authentication"];
+
+  if (token && token.length > 10 && token !== null) {
+    try {
+      req.activeUser = jwt.verify(
+        token,
+        process.env.encrypt,
+        (err, decoded) => {
+          return decoded;
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  next();
+});
+
+server.applyMiddleware({ app });
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: process.env.port }, () => {
+  console.log(`${process.env.localhost}${server.graphqlPath}`);
+  console.log(`${process.env.ws}${server.subscriptionsPath}`);
+});
